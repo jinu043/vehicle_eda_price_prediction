2e394f06ca55b9bd50a7f6cbfa888574f0af8292
Jineesh Bhaskaran <93069439+jinu043@users.noreply.github.com>
2023-09-04 20:19:56 +0400

    Update 3 📜SUMMARY.py

--- pages/3 📜SUMMARY.py
+++ pages/3 📜SUMMARY.py
@@ -5,10 +5,10 @@ st.set_page_config(layout="wide", page_icon="📜")
 st.markdown('<h2 div style="text-align: center;"> <mark style = "background-color:#F3EBEC;color:darkred;font-weight:bold">PROJECT SUMMARY</mark> </div>', unsafe_allow_html=True)
 
 st.markdown(f'<div style="text-align: justify;"><mark style = "background-color:#F3EBEC;color:#110301;font-family:verdana">\
-I have been used dubicars website used cars data for market price analysis and prediction of used cars in UAE market. As an exploratory data\
- analysis, I have shown the distribution of vehicle makes and models with respect to fuel type, model year, dealers etc and price distribution with\
- different similar features. I have also built a machine learning model to predict the price of used cars in UAE market. When you replace the data with\
- upcoming months vehicle details, we could get the respective distributions to boost our business analysis. </mark></div>', unsafe_allow_html=True)
+I have been used dubicars website `used cars` data for market price analysis and prediction of used cars in UAE market. As an exploratory data\
+ analysis, I have shown the distribution of vehicle makes and models with respect to fuel type, model year, dealers etc. I have also shown price distribution of different models, dealers and price relation with vehicle mileage. \
+ I have also built a machine learning model to predict the price of used cars in UAE market. When you replace the data with\
+ upcoming months vehicle details, we could get the respective months vehicle distributions to boost our business analysis. </mark></div>', unsafe_allow_html=True)
 
 st.markdown('<h3 div style="text-align: center;"> <mark style = "background-color:#F3EBEC;color:#DD9526;font-weight:bold">\
 Thank you 😊!!!</mark> </div>', unsafe_allow_html=True)
